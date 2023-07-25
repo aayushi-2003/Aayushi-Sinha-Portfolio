1eb58a72f5c4af6786535482a2bd3e9a132e3f20
Aayushi Sinha <98167798+aayushi-2003@users.noreply.github.com>
2023-07-25 20:00:28 +0530

    Added about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, {Suspense} from "react";
+import {Canvas} from "@react-three/fiber";
+import {OrbitControls, MeshDistortMaterial, Sphere} from "@react-three/drei";
+import {motion} from 'framer-motion';
+import {fadeIn} from '../variants';
 
 const About = () => {
     return (
       <div id="about" className="section">
-        <div className="container">
-          <div className="card-container w-1/2 flex items-center justify-center">
-            <div className="h-[400px] w-[400px] bg-blue-400 rounded-3xl bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 -rotate-6 relative ">
-            <div className="h-[380px] w-[380px] bg-white rounded-3xl absolute rotate-6 left-8 top-2 text-black p-16 ">
+        <div className="container flex justify-between items-center mx-auto gap-8">
+          <motion.div variants={fadeIn('down', 0.3)} initial="hidden" whileInView={'show'} className="card-container flex items-center justify-center p-10">
+            <div className="h-[400px] w-[400px] bg-blue-400 rounded-3xl bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 -rotate-6 relative -left-10">
+            <div className="h-[380px] w-[380px] bg-white rounded-3xl absolute rotate-6 left-10 top-2 text-black p-16 ">
               <h1 className="mb-4 text-[22px] font-semibold">Key Skills</h1>
               <div className="flex gap-10 font-medium leading-10 ">
               <ul className="tech-list">
@@ -26,7 +30,28 @@ const About = () => {
               </div>
               </div>
             </div>
+          </motion.div>
+          <motion.div variants={fadeIn('up', 0.3)} initial="hidden" whileInView={'show'}className="about-details flex flex-col items-end justify-center m-16 text-right gap-16 relative">
+            <div className="absolute z-0 -top-16 -right-[120px]">
+          <Canvas >
+            <Suspense fallback={null}>
+              <OrbitControls enableZoom={false}  />
+              <ambientLight intensity={1} />
+              <directionalLight position={[3, 2, 1]} />
+              <Sphere args={[1, 100, 200]} scale={3}>
+                <MeshDistortMaterial
+                  color="#4EA3F1"
+                  attach="material"
+                  distort={0.2}
+                  speed={2}
+                />
+              </Sphere>
+            </Suspense>
+          </Canvas>
           </div>
+            <h1 className="text-[60px] z-10">About Me</h1>
+            <p className="text-justify z-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, vel? Minima eaque quam vel iste id consequatur accusamus molestiae, quisquam aspernatur ut, inventore veniam, reprehenderit aliquid blanditiis in autem optio.</p>
+          </motion.div>
         </div>
       </div>
     );
